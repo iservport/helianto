3e1c11474e26ee21abb41613bec805db31313bb1
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-04-17 00:36:57 +0000

    added UserDaoImplTests and UserDetailsServiceTests
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@71 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto/core/src/main/java/org/helianto/core/security/PublicUserDetailsSwitcher.java
@@ -0,0 +1,47 @@
+/* Copyright 2005 I Serv Consultoria Empresarial Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.helianto.core.security;
+
+import java.util.List;
+
+import org.helianto.core.Entity;
+
+/**
+ * Implementations
+ * of this interface should provide code to
+ * switch from one <code>User</code> to another having the
+ * same <code>Credential</code>.
+ * 
+ * @author Mauricio Fernandes de Castro
+ */
+public interface PublicUserDetailsSwitcher extends PublicUserDetails {
+
+    /**
+     * Return an <code>Entity</code> from all
+     * <code>User</code>s connected by the same
+     * <code>Credential</code>.
+     */
+    public List<Entity> getEntities();
+    
+    /**
+     * Set an <code>Entity</code> if there is an 
+     * <code>User</code> that to replace the current
+     * secure <code>User</code> associated with the
+     * <code>Entity</code> with the same <code>Credential</code> .
+     */
+    public void setEntity(Entity entity);
+    
+}
