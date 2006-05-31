ce6d3bc66114ab7a8269897023649fc07a93f1a3
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-05-31 23:23:15 +0000

    added ResourceGroup tests
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@111 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto/process/src/main/java/org/helianto/process/hibernate/ResourceDaoImpl.java
@@ -0,0 +1,48 @@
+/* Copyright 2005 I Serv Consultoria Empresarial Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.helianto.process.hibernate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.helianto.core.Entity;
+import org.helianto.core.hibernate.GenericDaoImpl;
+import org.helianto.process.ResourceGroup;
+import org.helianto.process.dao.ResourceDao;
+
+public class ResourceDaoImpl extends GenericDaoImpl implements ResourceDao {
+
+    public void persistResourceGroup(ResourceGroup resourceGroup) {
+        persist(resourceGroup);
+    }
+    
+    public List<ResourceGroup> findResourceByEntity(Entity entity) {
+        List<ResourceGroup> resourceList = (ArrayList<ResourceGroup>) find(RESOURCEGROUP_QRY, entity);
+        return resourceList;
+    }
+    
+    public List<ResourceGroup> findResourceByParent(ResourceGroup resourceGroup) {
+        List<ResourceGroup> resourceList = (ArrayList<ResourceGroup>) find(RESOURCEGROUP_QRY_BYPARENT, resourceGroup);
+        return resourceList;
+    }
+    
+    static String RESOURCEGROUP_QRY = "from ResourceGroup resourceGroup " +
+    "where resourceGroup.entity = ?";
+
+    static String RESOURCEGROUP_QRY_BYPARENT = "from ResourceGroup resourceGroup " +
+    "where resourceGroup.parent = ?";
+
+}
