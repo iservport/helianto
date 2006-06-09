410de6225aa0bf1ccc7c16922ff9307a75c5d083
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-06-09 20:47:48 +0000

    refactored to package domain.process.creation
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@157 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto/domain/src/main/java/org/helianto/process/creation/PartCreatorImpl.java
@@ -0,0 +1,85 @@
+/* Copyright 2005 I Serv Consultoria Empresarial Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.helianto.process.creation;
+
+import org.helianto.core.Entity;
+import org.helianto.process.Document;
+import org.helianto.process.Part;
+import org.helianto.process.Tree;
+import org.helianto.process.Unit;
+import org.helianto.process.creation.AssociationType;
+
+/**
+ * Default implementation of <code>PartCreator</code> interface.
+ * 
+ * @author Mauricio Fernandes de Castro
+ * @version $Id: $
+ */
+public class PartCreatorImpl  implements PartCreator {
+
+    public Part partWithDrawingFactory(Entity entity, String drawingNumber, String drawingName) {
+    	return partFactory(entity, drawingNumber, drawingName, true);
+    }
+
+    public Part partFactory(Entity entity, String partNumber, String partName) {
+        return partFactory(entity, partNumber, partName, false);
+    }
+
+    /*
+     * internal part factory.
+     */
+    private Part partFactory(Entity entity, String partNumber, String partName, boolean hasDrawing) {
+        Part part = new Part();
+        part.setEntity(entity);
+        part.setDocCode(partNumber);
+        part.setDocName(partName);
+        part.setHasDrawing(hasDrawing);
+        return part;
+    }
+
+	public MaterialType materialTypeFactory(Unit materialUnit, String materialName) {
+		MaterialType materialType = new MaterialType();
+		materialType.setEntity(materialUnit.getEntity());
+		materialType.setMaterialUnit(materialUnit);
+		materialType.setMaterialName(materialName);
+		materialType.setInternalNumber(0);
+		return materialType;
+	}
+
+	public Part materialFactory(MaterialType materialType, String materialNumber, String materialName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Part componentFactory(Part parent, String drawingNumber, String drawingName, double coefficient) {
+		Part child = partFactory(parent.getEntity(), drawingNumber, drawingName);
+		Tree tree = associationFactory(parent, child, coefficient);
+		tree.setAssociationType(AssociationType.PART_PART.getValue());
+		return child;
+	}
+	
+	/*
+	 * Internal tree factory
+	 */
+	private Tree associationFactory(Document parent, Document child, double coefficient) {
+		Tree tree = new Tree();
+		tree.setParent(parent);
+		tree.setChild(child);
+		parent.getChildren().add(tree);
+		return tree;
+	}
+
+}
---
+++ helianto/domain/src/main/java/org/helianto/process/creation/ProcessCreator.java
@@ -0,0 +1,23 @@
+package org.helianto.process.creation;
+
+import org.helianto.process.Characteristic;
+import org.helianto.process.DocumentDetail;
+import org.helianto.process.Operation;
+import org.helianto.process.Part;
+import org.helianto.process.Process;
+
+public interface ProcessCreator {
+	
+	public Process createProcess(Part part, String processCode);
+	
+	public Process createProcess(Part part, String processCode, String processName);
+	
+	public Operation createOperation(Process process, String operationCode);
+	
+	public Operation createOperation(Process process, String operationCode, String operationName);
+	
+	public DocumentDetail createOperationDetail(Operation operation);
+	
+	public Characteristic createCharacteristic(Operation operation);
+
+}
