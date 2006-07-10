5b530c906e262ce577176fa9e3f26d4a01e05946
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-07-10 18:47:35 +0000

    moved types to a new package
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@242 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto/domain/src/main/java/org/helianto/core/type/CredentialState.java
@@ -0,0 +1,48 @@
+/* Copyright 2005 I Serv Consultoria Empresarial Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.helianto.core.type;
+
+import org.helianto.core.Credential;
+
+/**
+ * An enumeration to supply char types for 
+ * {@link Credential#credentialState}.
+ * 
+ * <p>
+ * Only active credentials should be granted authorization 
+ * into the system.
+ * </p>
+ * 
+ * @author Mauricio Fernandes de Castro
+ * @version $Id: CredentialState.java,v 1.1 2006/03/02 22:44:27 iserv Exp $
+ */
+public enum CredentialState {
+    
+    ACTIVE('A'),
+    CANCELLED('C'),
+    IDLE('I'),
+    SUSPENDED('S');
+    
+    private char value;
+    
+    private CredentialState(char value) {
+        this.value = value;
+    }
+    public char getValue() {
+        return this.value;
+    }
+
+}
