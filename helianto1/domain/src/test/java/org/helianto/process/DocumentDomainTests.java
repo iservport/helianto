b364f10e3db0860f8c3bf7db3eef099ac289e728
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-09-07 14:15:22 +0000

    added ExternalDocument
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@315 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto1/domain/src/test/java/org/helianto/process/DocumentDomainTests.java
@@ -0,0 +1,52 @@
+package org.helianto.process;
+
+import java.util.ArrayList;
+
+import org.helianto.core.Entity;
+import org.helianto.core.Identity;
+import org.helianto.core.test.DomainTestSupport;
+import org.helianto.process.type.DocumentType;
+
+import junit.framework.TestCase;
+
+public class DocumentDomainTests extends TestCase {
+	
+	public void testDocument() {
+		
+		Document document = new Document();
+		document.setId(Long.MAX_VALUE);
+		document.setId(Long.MIN_VALUE);
+		document.setEntity(new Entity());
+		document.setDocCode("");
+		document.setDocName("");
+		document.setChildren(new ArrayList<Tree>(0));
+	}
+	
+	public void testDocumentEquals() {
+		Document copy, document = new Document();
+		document.setEntity(new Entity());
+		document.setDocCode("TEST");
+        copy = (Document) DomainTestSupport.minimalEqualsTest(document);
+        
+        copy.setEntity(document.getEntity());
+        assertFalse(document.equals(copy));
+        
+        copy.setDocCode("TEST");
+        assertTrue(document.equals(copy));
+	}
+	
+	public void testExternalDocument() {
+		ExternalDocument externalDocument = new ExternalDocument();
+		externalDocument.setId(Long.MAX_VALUE);
+		externalDocument.setId(Long.MIN_VALUE);
+		externalDocument.setEntity(new Entity());
+		externalDocument.setDocCode("");
+		externalDocument.setDocName("");
+		externalDocument.setDocUrl("");
+		externalDocument.setDocType(DocumentType.CATEGORY.getValue());
+		externalDocument.setDocType(DocumentType.FILE.getValue());
+		externalDocument.setDocType(DocumentType.FOLDER.getValue());
+		externalDocument.setParent(new Document());
+	}
+
+}
