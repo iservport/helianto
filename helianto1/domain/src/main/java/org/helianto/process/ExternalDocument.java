9f03c4bc60e27657012446c15cf26eee1af77095
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-09-11 23:53:32 +0000

    regenerated
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@328 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto1/domain/src/main/java/org/helianto/process/ExternalDocument.java
@@ -0,0 +1,83 @@
+package org.helianto.process;
+// Generated 09/09/2006 21:08:17 by Hibernate Tools 3.1.0.beta4
+
+import java.util.List;
+import org.helianto.core.Entity;
+
+
+/**
+ * 			
+ * <p>
+ * Documents to be stored appart from the datastore.
+ * </p>
+ * @author Mauricio Fernandes de Castro
+ * 				
+ * 		
+ */
+
+public class ExternalDocument extends org.helianto.process.Document implements java.io.Serializable {
+
+
+    // Fields    
+
+     private String docUrl;
+     private char docType;
+     private ExternalDocument parent;
+
+
+    // Constructors
+
+    /** default constructor */
+    public ExternalDocument() {
+    }
+
+	/** minimal constructor */
+    public ExternalDocument(Entity entity, String docCode) {
+        super(entity, docCode);        
+    }
+    
+    /** full constructor */
+    public ExternalDocument(Entity entity, String docCode, String docName, List<Tree> children, String docUrl, char docType, ExternalDocument parent) {
+        super(entity, docCode, docName, children);        
+        this.docUrl = docUrl;
+        this.docType = docType;
+        this.parent = parent;
+    }
+    
+
+   
+    // Property accessors
+
+    public String getDocUrl() {
+        return this.docUrl;
+    }
+    
+    public void setDocUrl(String docUrl) {
+        this.docUrl = docUrl;
+    }
+
+    public char getDocType() {
+        return this.docType;
+    }
+    
+    public void setDocType(char docType) {
+        this.docType = docType;
+    }
+
+    public ExternalDocument getParent() {
+        return this.parent;
+    }
+    
+    public void setParent(ExternalDocument parent) {
+        this.parent = parent;
+    }
+   
+
+
+
+
+
+
+
+
+}
