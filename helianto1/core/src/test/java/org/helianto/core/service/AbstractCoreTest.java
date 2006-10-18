c5934775bd1b3a32f3b70c329042143391c53012
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-10-18 15:21:17 +0000

    unstable
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@347 d46e4f78-7810-0410-b419-9f95c2a9a517

--- helianto1/core/src/test/java/org/helianto/core/service/AbstractCoreTest.java
+++ helianto1/core/src/test/java/org/helianto/core/service/AbstractCoreTest.java
@@ -15,7 +15,7 @@
 
 package org.helianto.core.service;
 
-import org.helianto.core.junit.AbstractIntegrationTest;
+import org.helianto.core.test.AbstractIntegrationTest;
 
 public class AbstractCoreTest extends AbstractIntegrationTest {
 
