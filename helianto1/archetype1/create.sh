52ec89dfeba1704bcdbb430ded01eb2da5cec2b0
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-11-07 20:45:00 +0000

    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@443 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto1/archetype1/create.sh
@@ -0,0 +1,7 @@
+cd ~/workspace
+mvn archetype:create \
+-DarchetypeGroupId=org.helianto \
+-DarchetypeArtifactId=helianto-archetype \
+-DarchetypeVersion=0.0.3 \
+-DgroupId=org.helianto \
+-DartifactId=sample1
