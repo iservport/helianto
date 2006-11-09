a8863af01018750c3b76e7296e5e03f72da20290
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-11-09 19:04:30 +0000

    unstable
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@448 d46e4f78-7810-0410-b419-9f95c2a9a517

--- helianto1/web/src/test/java/org/helainto/web/view/PrincipalPageTests.java
+++ helianto1/web/src/test/java/org/helainto/web/view/PrincipalPageTests.java
@@ -20,8 +20,19 @@ import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import junit.framework.TestCase;
 
+import org.helianto.web.view.IdentityForm;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.web.context.support.StaticWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.support.BindStatus;
+import org.springframework.web.servlet.support.RequestContext;
+import org.springframework.web.servlet.theme.FixedThemeResolver;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import freemarker.template.Template;
@@ -29,44 +40,68 @@ import freemarker.template.TemplateException;
 
 public class PrincipalPageTests extends TestCase {
 
-    private FreeMarkerConfigurer configurer;
-    private Map<String, Object> model;
-    
-    public void testSummary() {
-        model.put("page", 1);
-        StringWriter result = process("identity/summary.ftl", false);
-    }
-    
-//    public void testPrincipal() {
-//        model.put("page", 1);
-//        StringWriter result = process("identity/principal.ftl", false);
-//    }
-    
-    public StringWriter process(String templateName, boolean visualTest) {
-        try {
-            Template template = 
-//                configurer.getTemplate(templateName);
-            configurer.getConfiguration().getTemplate(templateName);
-            StringWriter result = new StringWriter();
-            template.process(model, result);
-            if (visualTest) {
-                System.out.println(result.toString());
-                fail();
-            }
-            return result;
-        } catch (IOException e) {
-            throw new IllegalStateException("Unable to open template "+templateName, e);
-        } catch (TemplateException e) {
-            throw new IllegalStateException("Ivalid template "+templateName, e);
-        }
-    }
+	private FreeMarkerConfigurer configurer;
 
-    public void setUp() throws IOException, TemplateException {
-        configurer = new FreeMarkerConfigurer();
-        configurer.setTemplateLoaderPath("classpath:/freemarker/");
-        configurer.setDefaultEncoding("ISO-8859-1");
-        configurer.afterPropertiesSet();
-        model = new HashMap<String, Object>();
-  }
-    
+	private Map<String, Object> model;
+
+	private Map<String, Object> requestContext;
+
+	public void testSummary() {
+		model.put("page", 1);
+		StringWriter result = process("identity/summary.ftl", false);
+	}
+
+//	public void testPrincipal() {
+//		model.put("page", 1);
+//		StringWriter result = process("identity/principal.ftl", false);
+//	}
+
+	public StringWriter process(String templateName, boolean visualTest) {
+		try {
+			Template template =
+			// configurer.getTemplate(templateName);
+			configurer.getConfiguration().getTemplate(templateName);
+			StringWriter result = new StringWriter();
+			template.process(model, result);
+			if (visualTest) {
+				System.out.println(result.toString());
+				fail();
+			}
+			return result;
+		} catch (IOException e) {
+			throw new IllegalStateException("Unable to open template "
+					+ templateName, e);
+		} catch (TemplateException e) {
+			throw new IllegalStateException("Ivalid template " + templateName,
+					e);
+		}
+	}
+
+	public void setUp() throws IOException, TemplateException, InstantiationException, IllegalAccessException {
+		configurer = new FreeMarkerConfigurer();
+		configurer.setTemplateLoaderPath("classpath:/freemarker/");
+		configurer.setDefaultEncoding("ISO-8859-1");
+		configurer.afterPropertiesSet();
+		model = prepareForm(IdentityForm.class, "identityForm");
+	}
+	
+	public Map<String, Object> prepareForm(Class formObjectClass, String formObjectName) throws InstantiationException, IllegalAccessException {
+		Map<String, Object> model = new HashMap<String, Object>();
+		StaticWebApplicationContext wac = new StaticWebApplicationContext();
+		wac.setServletContext(new MockServletContext());
+		HttpServletRequest request = new MockHttpServletRequest();
+		request.setAttribute(
+				DispatcherServlet.WEB_APPLICATION_CONTEXT_ATTRIBUTE, wac);
+		request.setAttribute(DispatcherServlet.LOCALE_RESOLVER_ATTRIBUTE,
+				new AcceptHeaderLocaleResolver());
+		request.setAttribute(DispatcherServlet.THEME_RESOLVER_ATTRIBUTE,
+				new FixedThemeResolver());
+		RequestContext requestContext = new RequestContext(request);
+//		BindStatus status = requestContext.getBindStatus("tb.name");
+		BindStatus status = new BindStatus(requestContext, formObjectName, false);
+		model.put("springMacroRequestContext", requestContext);
+		model.put(formObjectName, formObjectClass.newInstance());
+		return model;
+	}
+
 }
