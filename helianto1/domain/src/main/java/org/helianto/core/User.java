bb87ca294b43cb85792b12ba1c3624b428c14958
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-12-07 10:53:06 +0000

    regenerated
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@500 d46e4f78-7810-0410-b419-9f95c2a9a517

--- helianto1/domain/src/main/java/org/helianto/core/User.java
+++ helianto1/domain/src/main/java/org/helianto/core/User.java
@@ -1,5 +1,5 @@
 package org.helianto.core;
-// Generated 05/12/2006 07:28:32 by Hibernate Tools 3.2.0.beta8
+// Generated 07/12/2006 07:44:31 by Hibernate Tools 3.2.0.beta8
 
 
 import java.util.Set;
