364e5c032255eabb37f64a87344e35e28a3dd95a
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2006-12-08 09:08:24 +0000

    added to allow multiple groups per user
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@504 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto1/domain/src/main/java/org/helianto/core/UserAssociation.java
@@ -0,0 +1,81 @@
+package org.helianto.core;
+// Generated 07/12/2006 16:03:04 by Hibernate Tools 3.2.0.beta8
+
+
+
+/**
+ * 				
+ * <p>
+ * A class to hold user and user groups associations.
+ * </p>
+ * @author Mauricio Fernandes de Castro
+ * 				
+ * 		
+ */
+public class UserAssociation  implements java.io.Serializable {
+
+    // Fields    
+
+     private long id;
+     private UserGroup parent;
+     private UserGroup child;
+
+     // Constructors
+
+    /** default constructor */
+    public UserAssociation() {
+    }
+
+    /** full constructor */
+    public UserAssociation(UserGroup parent, UserGroup child) {
+       this.parent = parent;
+       this.child = child;
+    }
+   
+    // Property accessors
+    public long getId() {
+        return this.id;
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+    public UserGroup getParent() {
+        return this.parent;
+    }
+    
+    public void setParent(UserGroup parent) {
+        this.parent = parent;
+    }
+    public UserGroup getChild() {
+        return this.child;
+    }
+    
+    public void setChild(UserGroup child) {
+        this.child = child;
+    }
+
+
+   public boolean equals(Object other) {
+         if ( (this == other ) ) return true;
+		 if ( (other == null ) ) return false;
+		 if ( !(other instanceof UserAssociation) ) return false;
+		 UserAssociation castOther = ( UserAssociation ) other; 
+         
+		 return ( (this.getParent()==castOther.getParent()) || ( this.getParent()!=null && castOther.getParent()!=null && this.getParent().equals(castOther.getParent()) ) )
+ && ( (this.getChild()==castOther.getChild()) || ( this.getChild()!=null && castOther.getChild()!=null && this.getChild().equals(castOther.getChild()) ) );
+   }
+   
+   public int hashCode() {
+         int result = 17;
+         
+         
+         result = 37 * result + ( getParent() == null ? 0 : this.getParent().hashCode() );
+         result = 37 * result + ( getChild() == null ? 0 : this.getChild().hashCode() );
+         return result;
+   }   
+
+
+}
+
+
