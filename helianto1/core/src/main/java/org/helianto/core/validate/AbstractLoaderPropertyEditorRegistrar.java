9f16f32edd63d9ac8acacce7449f4d3b174bcc31
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2007-03-27 16:31:46 +0000

    added Abstract class to support PropertyLoader backed property editor registrars
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@583 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto1/core/src/main/java/org/helianto/core/validate/AbstractLoaderPropertyEditorRegistrar.java
@@ -0,0 +1,49 @@
+/* Copyright 2005 I Serv Consultoria Empresarial Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.helianto.core.validate;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.PropertyEditorRegistrar;
+
+/**
+ * A base class to <code>PropertyEditorRegistrar</code> that
+ * inject an <code>PropertyLoader</code> via <code>CustomPropertyEditor</code>
+ * constructors.
+ * 
+ * @author Mauricio Fernandes de Castro
+ */
+public abstract class AbstractLoaderPropertyEditorRegistrar implements PropertyEditorRegistrar {
+
+    private PropertyLoader propertyLoader;
+    
+    public void setPropertyLoader(PropertyLoader propertyLoader) {
+        this.propertyLoader = propertyLoader;
+    }
+
+    public PropertyLoader getPropertyLoader() {
+        return propertyLoader;
+    }
+    
+    public void init() {
+        if (propertyLoader==null) {
+            throw new IllegalArgumentException("PropertyLoader requierd...");
+        }
+    }
+
+    public static final Log logger = LogFactory.getLog(AbstractLoaderPropertyEditorRegistrar.class);
+
+}
