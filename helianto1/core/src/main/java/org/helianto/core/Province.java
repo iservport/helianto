e4ede8679dcec9a946d1be66930f47f71772a513
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2007-04-13 14:09:51 +0000

    moved Province from core module
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@645 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto1/core/src/main/java/org/helianto/core/Province.java
@@ -0,0 +1,122 @@
+/* Copyright 2005 I Serv Consultoria Empresarial Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.helianto.core;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * <p>
+ * Provinces.
+ * </p>
+ * @author Mauricio Fernandes de Castro
+ */
+@javax.persistence.Entity
+@Table(name="core_province",
+    uniqueConstraints = {@UniqueConstraint(columnNames={"operatorId", "code"})}
+)
+public class Province  implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private Operator operator;
+    private String code;
+    private String provinceName;
+
+    /** default constructor */
+    public Province() {
+    }
+
+    // Property accessors
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name="operatorId", nullable=true)
+    public Operator getOperator() {
+        return this.operator;
+    }
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+    
+    @Column(length=3)
+    public String getCode() {
+        return this.code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
+    @Column(length=32)
+    public String getProvinceName() {
+        return this.provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    /**
+     * toString
+     * @return String
+     */
+     public String toString() {
+	  StringBuffer buffer = new StringBuffer();
+
+      buffer.append(getClass().getName()).append("@").append(Integer.toHexString(hashCode())).append(" [");
+      buffer.append("operator").append("='").append(getOperator()).append("' ");			
+      buffer.append("code").append("='").append(getCode()).append("' ");			
+      buffer.append("]");
+      
+      return buffer.toString();
+     }
+
+   public boolean equals(Object other) {
+         if ( (this == other ) ) return true;
+		 if ( (other == null ) ) return false;
+		 if ( !(other instanceof Province) ) return false;
+		 Province castOther = ( Province ) other; 
+         
+		 return ( (this.getOperator()==castOther.getOperator()) || ( this.getOperator()!=null && castOther.getOperator()!=null && this.getOperator().equals(castOther.getOperator()) ) )
+ && ( (this.getCode()==castOther.getCode()) || ( this.getCode()!=null && castOther.getCode()!=null && this.getCode().equals(castOther.getCode()) ) );
+   }
+   
+   public int hashCode() {
+         int result = 17;
+         
+         
+         result = 37 * result + ( getOperator() == null ? 0 : this.getOperator().hashCode() );
+         result = 37 * result + ( getCode() == null ? 0 : this.getCode().hashCode() );
+         
+         return result;
+   }   
+
+
+}
+
+
