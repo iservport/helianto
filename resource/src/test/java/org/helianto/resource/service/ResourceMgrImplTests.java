1f0881327a37a6aa7f8ccdcd36cd9ffd02c8ec72
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2009-07-08 00:29:36 +0000

    added resource tests
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@1189 d46e4f78-7810-0410-b419-9f95c2a9a517

--- resource/src/test/java/org/helianto/resource/service/ResourceMgrImplTests.java
+++ resource/src/test/java/org/helianto/resource/service/ResourceMgrImplTests.java
@@ -1,24 +1,90 @@
 package org.helianto.resource.service;
 
 import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.reset;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
-import org.helianto.resource.service.ResourceMgrImpl;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import org.helianto.core.dao.FilterDao;
+import org.helianto.resource.ResourceAssociation;
+import org.helianto.resource.ResourceGroup;
+import org.helianto.resource.ResourceGroupFilter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import junit.framework.TestCase;
-
-public class ResourceMgrImplTests extends TestCase {
+/**
+ * @author Mauricio Fernandes de Castro
+ */
+public class ResourceMgrImplTests {
 	
-	// class (interface) under test
-	private ResourceMgrImpl resourceMgr;
-	
-	public void test() {
+	@Test
+	public void testFindResourceGroups() {
+		List<ResourceGroup> resourceGroupList = new ArrayList<ResourceGroup>();
+		ResourceGroupFilter filter = new ResourceGroupFilter();
 		
+		expect(resourceGroupDao.find(filter)).andReturn(resourceGroupList);
+		replay(resourceGroupDao);
+		
+		assertSame(resourceGroupList, resourceMgr.findResourceGroups(filter));
+		verify(resourceGroupDao);
 	}
 	
-	@Override
+	@SuppressWarnings("serial")
+	@Test
+	public void testPrepareResourceGroup() {
+		ResourceGroup resourceGroup = new ResourceGroup();
+		ResourceGroup managedResourceGroup = new ResourceGroup() {
+			@Override public Set<ResourceAssociation> getChildAssociations() {
+				setId(1);
+				return null;
+			}
+		};
+		
+		expect(resourceGroupDao.merge(resourceGroup)).andReturn(managedResourceGroup);
+		resourceGroupDao.evict(resourceGroup);
+		replay(resourceGroupDao);
+		
+		assertSame(managedResourceGroup, resourceMgr.prepareResourceGroup(resourceGroup));
+		assertEquals(1, managedResourceGroup.getId());
+		verify(resourceGroupDao);
+	}
+	
+	@Test
+	public void testStoreResourceGroup() {
+		ResourceGroup resourceGroup = new ResourceGroup();
+		ResourceGroup managedResourceGroup = new ResourceGroup();
+		
+		expect(resourceGroupDao.merge(resourceGroup)).andReturn(managedResourceGroup);
+		replay(resourceGroupDao);
+		
+		assertSame(managedResourceGroup, resourceMgr.storeResourceGroup(resourceGroup));
+		verify(resourceGroupDao);
+	}
+	
+	// collabs
+	
+	private ResourceMgrImpl resourceMgr;
+	private FilterDao<ResourceGroup, ResourceGroupFilter> resourceGroupDao;
+	
+	@SuppressWarnings("unchecked")
+	@Before
 	public void setUp() {
 		resourceMgr = new ResourceMgrImpl();
+		resourceGroupDao = createMock(FilterDao.class);
+		resourceMgr.setResourceGroupDao(resourceGroupDao);
+	}
+	
+	@After
+	public void tearDown() {
+		reset(resourceGroupDao);
 	}
 	
 
