fedfd46bfe381143584a1dfc7764f3c29be80b85
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2009-08-29 19:27:36 +0000

    refactored partner daos to java config
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@1263 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ partner/src/main/java/org/helianto/partner/PartnerRepositoryConfiguration.java
@@ -0,0 +1,126 @@
+/* Copyright 2005 I Serv Consultoria Empresarial Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.helianto.partner;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.helianto.core.dao.BasicDao;
+import org.helianto.core.dao.FilterDao;
+import org.helianto.core.repository.CoreHibernateRepository;
+import org.helianto.core.repository.RepositoryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Java config to the repository.
+ * 
+ * @author Mauricio Fernandes de Castro
+ */
+@Configuration
+@SuppressWarnings("unchecked")
+public class PartnerRepositoryConfiguration {
+	
+	/**
+	 * Account data access.
+	 */
+	@Bean
+	public FilterDao<Account, AccountFilter> accountDao() {
+		FilterDao<Account, AccountFilter> dao =  
+			repositoryFactory.filterDaoFactory(Account.class, AccountFilter.class, "entity", "accountCode");
+		logger.info("Created accountDao");
+		return dao;
+	}
+
+	/**
+	 * Address data access.
+	 */
+	@Bean
+	public BasicDao<Address> addressDao() {
+		BasicDao<Address> dao =  
+			repositoryFactory.basicDaoFactory(Address.class, "partnerRegistry", "sequence");
+		logger.info("Created addressDao");
+		return dao;
+	}
+
+	/**
+	 * Partner data access.
+	 */
+	@Bean
+	public FilterDao<Partner, PartnerFilter> partnerDao() {
+		FilterDao<Partner, PartnerFilter> dao =  
+			repositoryFactory.filterDaoFactory(Partner.class, PartnerFilter.class, "partnerRegistry", "class");
+		logger.info("Created partnerDao");
+		return dao;
+	}
+
+	/**
+	 * Partner key data access.
+	 */
+	@Bean
+	public BasicDao<PartnerKey> partnerKeyDao() {
+		BasicDao<PartnerKey> dao =  
+			repositoryFactory.basicDaoFactory(PartnerKey.class, "partner", "keyType");
+		logger.info("Created partnerKeyDao");
+		return dao;
+	}
+
+	/**
+	 * Partner registry data access.
+	 */
+	@Bean
+	public FilterDao<PartnerRegistry, PartnerRegistryFilter> partnerRegistryDao() {
+		FilterDao<PartnerRegistry, PartnerRegistryFilter> dao =  
+			repositoryFactory.filterDaoFactory(PartnerRegistry.class, PartnerRegistryFilter.class, "entity", "partnerAlias");
+		logger.info("Created partnerRegistryDao");
+		return dao;
+	}
+
+	/**
+	 * Partner registry key data access.
+	 */
+	@Bean
+	public BasicDao<PartnerRegistryKey> partnerRegistryKeyDao() {
+		BasicDao<PartnerRegistryKey> dao =  
+			repositoryFactory.basicDaoFactory(PartnerRegistryKey.class, "partnerRegistry", "keyType");
+		logger.info("Created partnerRegistryKeyDao");
+		return dao;
+	}
+
+	/**
+	 * Phone data access.
+	 */
+	@Bean
+	public BasicDao<Phone> phoneDao() {
+		BasicDao<Phone> dao =  
+			repositoryFactory.basicDaoFactory(Phone.class, "address", "sequence");
+		logger.info("Created phoneDao");
+		return dao;
+	}
+
+	// collabs
+    
+    private RepositoryFactory repositoryFactory;
+    
+    @Resource
+	public void setRepositoryFactory(RepositoryFactory repositoryFactory) {
+		this.repositoryFactory = repositoryFactory;
+	}
+
+    private static final Log logger = LogFactory.getLog(CoreHibernateRepository.class);
+
+}
---
+++ partner/src/test/java/org/helianto/partner/PartnerIntegrationTest.java
@@ -0,0 +1,127 @@
+/* Copyright 2005 I Serv Consultoria Empresarial Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.helianto.partner;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.annotation.Resource;
+
+import org.helianto.core.dao.BasicDao;
+import org.helianto.core.dao.FilterDao;
+import org.helianto.partner.test.AbstractPartnerDaoIntegrationTest;
+import org.helianto.partner.test.AccountTestSupport;
+import org.helianto.partner.test.AddressTestSupport;
+import org.helianto.partner.test.AgentTestSupport;
+import org.helianto.partner.test.CustomerTestSupport;
+import org.helianto.partner.test.DivisionTestSupport;
+import org.helianto.partner.test.LaboratoryTestSupport;
+import org.helianto.partner.test.ManufacturerTestSupport;
+import org.helianto.partner.test.PartnerKeyTestSupport;
+import org.helianto.partner.test.PartnerRegistryKeyTestSupport;
+import org.helianto.partner.test.PartnerRegistryTestSupport;
+import org.helianto.partner.test.PartnerTestSupport;
+import org.helianto.partner.test.PhoneTestSupport;
+import org.helianto.partner.test.SupplierTestSupport;
+import org.junit.Test;
+
+
+/**
+ * 
+ * @author Mauricio Fernandes de Castro
+ */
+public class PartnerIntegrationTest extends AbstractPartnerDaoIntegrationTest {
+
+	@Resource FilterDao<Account, AccountFilter> accountDao;
+	@Test
+	public void account() {
+		Account target = AccountTestSupport.createAccount();
+		assertEquals(accountDao.merge(target), accountDao.findUnique(target.getEntity(), target.getAccountCode()));
+	}
+	
+	@Resource BasicDao<Address> addressDao;
+	@Test
+	public void address() {
+		Address target = AddressTestSupport.createAddress();
+		assertEquals(addressDao.merge(target), addressDao.findUnique(target.getPartnerRegistry(), target.getSequence()));
+	}
+	
+	@Resource FilterDao<Partner, PartnerFilter> partnerDao;
+	@Test
+	public void partner() {
+		Partner target = PartnerTestSupport.createPartner();
+		assertEquals(partnerDao.merge(target), partnerDao.findUnique(target.getPartnerRegistry(), 'P'));
+	}
+	// subclasses
+	@Test
+	public void agent() {
+		Agent target = AgentTestSupport.createAgent();
+		assertEquals(partnerDao.merge(target), partnerDao.findUnique(target.getPartnerRegistry(), 'A'));
+	}
+	@Test
+	public void customer() {
+		Customer target = CustomerTestSupport.createCustomer();
+		assertEquals(partnerDao.merge(target), partnerDao.findUnique(target.getPartnerRegistry(), 'C'));
+	}
+	@Test
+	public void division() {
+		Division target = DivisionTestSupport.createDivision();
+		assertEquals(partnerDao.merge(target), partnerDao.findUnique(target.getPartnerRegistry(), 'D'));
+	}
+	@Test
+	public void laboratory() {
+		Laboratory target = LaboratoryTestSupport.createLaboratory();
+		assertEquals(partnerDao.merge(target), partnerDao.findUnique(target.getPartnerRegistry(), 'L'));
+	}
+	@Test
+	public void manufacturer() {
+		Manufacturer target = ManufacturerTestSupport.createManufacturer();
+		assertEquals(partnerDao.merge(target), partnerDao.findUnique(target.getPartnerRegistry(), 'M'));
+	}
+	@Test
+	public void supplier() {
+		Supplier target = SupplierTestSupport.createSupplier();
+		assertEquals(partnerDao.merge(target), partnerDao.findUnique(target.getPartnerRegistry(), 'S'));
+	}
+	
+	@Resource BasicDao<PartnerKey> partnerKeyDao;
+	@Test
+	public void partnerKey() {
+		PartnerKey target = PartnerKeyTestSupport.createPartnerKey();
+		assertEquals(partnerKeyDao.merge(target), partnerKeyDao.findUnique(target.getPartner(), target.getKeyType()));
+	}
+
+	@Resource FilterDao<PartnerRegistry, PartnerRegistryFilter> partnerRegistryDao;
+	@Test
+	public void partnerRegistry() {
+		PartnerRegistry target = PartnerRegistryTestSupport.createPartnerRegistry();
+		assertEquals(partnerRegistryDao.merge(target), partnerRegistryDao.findUnique(target.getEntity(), target.getPartnerAlias()));
+	}
+	
+	@Resource BasicDao<PartnerRegistryKey> partnerRegistryKeyDao;
+	@Test
+	public void partnerRegistryKey() {
+		PartnerRegistryKey target = PartnerRegistryKeyTestSupport.createPartnerRegistryKey();
+		assertEquals(partnerRegistryKeyDao.merge(target), partnerRegistryKeyDao.findUnique(target.getPartnerRegistry(), target.getKeyType()));
+	}
+
+	@Resource BasicDao<Phone> phoneDao;
+	@Test
+	public void phone() {
+		Phone target = PhoneTestSupport.createPhone();
+		assertEquals(phoneDao.merge(target), phoneDao.findUnique(target.getAddress(), target.getSequence()));
+	}
+
+}
