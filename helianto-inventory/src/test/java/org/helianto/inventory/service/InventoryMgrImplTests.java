c5baba59f3c1c7566e2b6c1ba662ff9d63ce589a
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2010-11-26 22:09:02 +0000

    Tests
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@1550 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto-inventory/src/test/java/org/helianto/inventory/service/InventoryMgrImplTests.java
@@ -0,0 +1,83 @@
+package org.helianto.inventory.service;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.reset;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertSame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.helianto.core.Entity;
+import org.helianto.core.filter.ListFilter;
+import org.helianto.core.repository.FilterDao;
+import org.helianto.core.service.SequenceMgr;
+import org.helianto.inventory.ProcessAgreement;
+import org.helianto.inventory.ProcessRequirement;
+import org.helianto.inventory.ProcessRequirementFilter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author mauriciofernandesdecastro
+ */
+public class InventoryMgrImplTests {
+
+	@Test
+	public void findProcessRequirements() {
+		List<ProcessRequirement> requirementList = new ArrayList<ProcessRequirement>();
+		ListFilter filter = new ProcessRequirementFilter(new Entity());
+		
+		expect(processRequirementDao.find(filter)).andReturn(requirementList);
+		replay(processRequirementDao);
+		
+		assertSame(requirementList, inventoryMgr.findProcessRequirements(filter));
+		verify(processRequirementDao);
+	}
+	
+	@Test
+	public void storeProcessRequirement() {
+		ProcessRequirement requirement = new ProcessRequirement();
+		
+		sequenceMgr.validateInternalNumber(requirement);
+		replay(sequenceMgr);
+		
+		processRequirementDao.saveOrUpdate(requirement);
+		replay(processRequirementDao);
+		
+		assertSame(requirement, inventoryMgr.storeProcessRequirement(requirement));
+		verify(processRequirementDao);
+	}
+	
+	// collabs
+	
+	private InventoryMgrImpl inventoryMgr;
+	private FilterDao<ProcessRequirement, ListFilter> processRequirementDao;
+	private FilterDao<ProcessAgreement, ListFilter> agreementDao;
+	private SequenceMgr sequenceMgr;
+	
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		inventoryMgr = new InventoryMgrImpl();
+		processRequirementDao = createMock(FilterDao.class);
+		inventoryMgr.setProcessRequirementDao(processRequirementDao);
+		agreementDao = createMock(FilterDao.class);
+		inventoryMgr.setAgreementDao(agreementDao);
+		sequenceMgr = createMock(SequenceMgr.class);
+		inventoryMgr.setSequenceMgr(sequenceMgr);
+	}
+	
+	@After
+	public void tearDown() {
+		reset(processRequirementDao);
+		reset(agreementDao);
+		reset(sequenceMgr);
+	}
+	
+
+}
