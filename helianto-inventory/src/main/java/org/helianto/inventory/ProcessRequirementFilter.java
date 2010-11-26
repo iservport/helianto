063bfec63feaaaa70c753edeaf2d89ccb7834236
iserv <iserv@d46e4f78-7810-0410-b419-9f95c2a9a517>
2010-11-26 21:50:09 +0000

    Added process requirement filter
    git-svn-id: https://helianto.svn.sourceforge.net/svnroot/helianto/trunk@1547 d46e4f78-7810-0410-b419-9f95c2a9a517

---
+++ helianto-inventory/src/main/java/org/helianto/inventory/ProcessRequirementFilter.java
@@ -0,0 +1,80 @@
+package org.helianto.inventory;
+
+import org.helianto.core.Entity;
+import org.helianto.core.User;
+import org.helianto.core.filter.CriteriaBuilder;
+import org.helianto.document.AbstractControlFilter;
+
+
+/**
+ * Um filtro para seleção de ordens internas.
+ * 
+ * @author Mauricio Fernandes de Castro
+ */
+public class ProcessRequirementFilter extends AbstractControlFilter {
+
+	private static final long serialVersionUID = 1L;
+	private Class<? extends ProcessRequirement> clazz;
+	
+	/**
+	 * Empty constructor
+	 */
+	ProcessRequirementFilter() {
+		super();
+		setClazz(ProcessRequirement.class);
+	}
+
+	/**
+	 * Entity constructor.
+	 * 
+	 * @param entity
+	 */
+	public ProcessRequirementFilter(Entity entity) {
+		this(entity, 0);
+	}
+
+	/**
+	 * User constructor.
+	 * 
+	 * @param user
+	 */
+	public ProcessRequirementFilter(User user) {
+		this(user.getEntity(), 0);
+		setUser(user);
+	}
+
+	/**
+	 * Key constructor.
+	 * 
+	 * @param entity
+	 * @param internalNumber
+	 */
+	public ProcessRequirementFilter(Entity entity, long internalNumber) {
+		super();
+		setEntity(entity);
+		setInternalNumber(internalNumber);
+		setClazz(null);
+		setDateFieldName("requirementDate");
+		setToDate(null);
+	}
+	
+	@Override
+	protected void doFilter(CriteriaBuilder mainCriteriaBuilder) {
+		super.doFilter(mainCriteriaBuilder);
+		if (getClazz()!=null) {
+			mainCriteriaBuilder.appendAnd().append(getClazz());
+		}
+		appendOrderBy("requirementDate", mainCriteriaBuilder);
+	}
+	
+	/**
+	 * Type filter.
+	 */
+	public Class<? extends ProcessRequirement> getClazz() {
+		return clazz;
+	}
+	public void setClazz(Class<? extends ProcessRequirement> clazz) {
+		this.clazz = clazz;
+	}
+
+}
