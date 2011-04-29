fa6ff7a357aed60e48e48613557e1e00079bcd3a
Mauricio Fernandes de Castro <mauricio@iservport.com>
2011-04-29 14:28:32 -0300

    Test.

---
+++ helianto-core/src/test/java/org/helianto/core/filter/IdentityFilterAdapterTests.java
@@ -0,0 +1,60 @@
+/* Copyright 2005 I Serv Consultoria Empresarial Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.helianto.core.filter;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.helianto.core.Identity;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Mauricio Fernandes de Castro
+ */
+public class IdentityFilterAdapterTests {
+
+    public static String C1 = "";
+    public static String C2 = "alias.principal = 'principal' ";
+    public static String C3 = "lower(alias.personalData.firstName) like '%first%' ";
+
+    @Test
+    public void empty() {
+        assertEquals(C1, filter.createCriteriaAsString());
+    }
+    
+    @Test
+    public void select() {
+    	form.setPrincipal("PRINCIPAL");
+        assertEquals(C2, filter.createCriteriaAsString());
+    }
+    
+    @Test
+    public void filter() {
+    	form.getPersonalData().setFirstName("FIRST");
+        assertEquals(C1+C3, filter.createCriteriaAsString());
+    }
+    
+    private IdentityFilterAdapter filter;
+    private Identity form;
+    
+    @Before
+    public void setUp() {
+    	form = new Identity("");
+    	filter = new IdentityFilterAdapter(form);
+    }
+}
