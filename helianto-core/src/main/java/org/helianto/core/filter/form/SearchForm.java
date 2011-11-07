50a6957e76bdd8cfb16a964fc7b02d430cf9cc96
Mauricio Fernandes de Castro <mauricio@iservport.com>
2011-11-07 09:14:29 -0200

    Added R/W field.

--- helianto-core/src/main/java/org/helianto/core/filter/form/SearchForm.java
+++ helianto-core/src/main/java/org/helianto/core/filter/form/SearchForm.java
@@ -11,10 +11,24 @@ public interface SearchForm {
 	 * Search string.
 	 */
 	public String getSearchString();
+	
+	/**
+	 * Search string as R/W property.
+	 * 
+	 * @param searchString
+	 */
+	public void setSearchString(String searchString);
 
 	/**
 	 * Search list.
 	 */
 	public String getSearchList();
+
+	/**
+	 * Search list as R/W property.
+	 * 
+	 * @param searchList
+	 */
+	public void setSearchList(String searchList);
 
 }
