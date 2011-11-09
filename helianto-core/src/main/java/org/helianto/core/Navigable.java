3cc84e6e59ed693cc9793039cb9a2f33d210f2ea
Mauricio Fernandes de Castro <mauricio@iservport.com>
2011-11-09 19:16:39 -0200

    New Navigable interface.

---
+++ helianto-core/src/main/java/org/helianto/core/Navigable.java
@@ -0,0 +1,15 @@
+package org.helianto.core;
+
+/**
+ * Indicates instances as nodes of a hierarchical tree.
+ * 
+ * @author mauriciofernandesdecastro
+ */
+public interface Navigable {
+	
+	/**
+	 * Node path.
+	 */
+	public String getNodePath();
+
+}
