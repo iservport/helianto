d4effb0786bedd47e13dac36969ef5cb5bdd9723
Mauricio Fernandes de Castro <mauricio@iservport.com>
2011-11-14 11:01:09 -0200

    Added Child Form.

---
+++ helianto-core/src/main/java/org/helianto/core/filter/form/ChildForm.java
@@ -0,0 +1,31 @@
+package org.helianto.core.filter.form;
+
+/**
+ * Child form interface.
+ * 
+ * @author mauriciofernandesdecastro
+ */
+public interface ChildForm<C> {
+	
+	/**
+	 * Child constraint filters.
+	 */
+	public C getChild();
+	
+	/**
+	 * Child as a R/W property.
+	 * 
+	 * @param child
+	 */
+	public void setChild(C child);
+	
+	/**
+	 * The child filter name.
+	 */
+	public String getChildName();
+
+	/**
+	 * The child filter id field.
+	 */
+	public long getChildId();
+}
