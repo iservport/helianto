f0a5d9a18299c561b09eaa270b5c89ca6921476e
Mauricio Fernandes de Castro <mauricio@iservport.com>
2012-02-05 12:50:25 -0200

    Added Archivable interface.

---
+++ helianto-core/src/main/java/org/helianto/core/Archivable.java
@@ -0,0 +1,15 @@
+package org.helianto.core;
+
+/**
+ * Archive interface.
+ * 
+ * @author mauriciofernandesdecastro
+ */
+public interface Archivable {
+	
+	/**
+	 * Archive type.
+	 */
+	char getArchive();
+
+}
