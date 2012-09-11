122d693c492cd6263330c55378001ade521c519f
Mauricio Fernandes de Castro <mauricio@iservport.com>
2012-09-11 20:36:12 -0300

    Simplified search form.

--- helianto-core/src/main/java/org/helianto/core/filter/form/SearchForm.java
+++ helianto-core/src/main/java/org/helianto/core/filter/form/SearchForm.java
@@ -13,34 +13,8 @@ public interface SearchForm {
 	String getSearchString();
 	
 	/**
-	 * Search string as R/W property.
-	 * 
-	 * @param searchString
-	 */
-	void setSearchString(String searchString);
-	
-	/**
 	 * Search mode.
 	 */
 	char getSearchMode();
-
-	/**
-	 * Search mode as R/W property.
-	 * 
-	 * @param searchMode
-	 */
-	void setSearchMode(char searchMode);
-
-	/**
-	 * Search list.
-	 */
-	String getSearchList();
-
-	/**
-	 * Search list as R/W property.
-	 * 
-	 * @param searchList
-	 */
-	void setSearchList(String searchList);
 
 }
