f9e0eb23db80c28d7b4c0fc3efb273d55b349907
Mauricio Fernandes de Castro <mauricio@iservport.com>
2013-08-08 05:35:52 -0300

    Update java mail message adapter.

--- helianto-message/src/main/java/org/helianto/message/sender/JavaMailMessageAdapter.java
+++ helianto-message/src/main/java/org/helianto/message/sender/JavaMailMessageAdapter.java
@@ -1,10 +1,21 @@
 package org.helianto.message.sender;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+
+import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import org.helianto.core.domain.Identity;
 import org.helianto.message.AbstractMessageAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
 /**
@@ -13,7 +24,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
  * @author mauriciofernandesdecastro
  */
 public class JavaMailMessageAdapter extends AbstractMessageAdapter<MimeMessage> {
-	
+
 	/**
 	 * Java mail message constructor.
 	 * 
@@ -22,7 +33,7 @@ public class JavaMailMessageAdapter extends AbstractMessageAdapter<MimeMessage> 
 	public JavaMailMessageAdapter() {
 		super();
 	}
-	
+
 	/**
 	 * Adapter method.
 	 */
@@ -30,21 +41,81 @@ public class JavaMailMessageAdapter extends AbstractMessageAdapter<MimeMessage> 
 		String[] toArray = new String[getTo().size()];
 		return toArray;
 	}
-	
+
 	public MimeMessage getMessage() {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
 		try {
-			MimeMessageHelper helper = new MimeMessageHelper(getMessage(), true);
-			helper.setTo(getToAsStringArray());
+			helper = new MimeMessageHelper(mimeMessage, true);
+			addRecipents(getTo(), RecipientType.TO);
+			addRecipents(getCc(), RecipientType.CC);
+			addRecipents(getBcc(), RecipientType.BCC);
 			helper.setFrom(getFrom().getPrincipal());
 			helper.setSubject(getSubject());
 			helper.setText(getHtml(), true);
-			for (Resource file: getAttachments()) {
+			for (Resource file : getAttachments()) {
 				helper.addAttachment(file.getFilename(), file);
 			}
-			return getMessage();
+			return helper.getMimeMessage();
 		} catch (MessagingException e) {
 			throw new IllegalArgumentException();
 		}
 	}
-	
+
+	public String readFile(String filePath) {
+		DefaultResourceLoader loader = new DefaultResourceLoader();
+		Resource resource = loader.getResource("file:" + filePath);
+		String html = "";
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(
+					resource.getFile()));
+			String linha = "";
+			while ((linha = bufferedReader.readLine()) != null) {
+				html = html + linha;
+			}
+			bufferedReader.close();
+			return html;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	/**
+	 * Iterate to add recipients.
+	 * 
+	 * @param recipients
+	 * @param recipientType
+	 */
+	public void addRecipents(Set<Identity> recipients, RecipientType recipientType) {
+		for (Identity identity: recipients) {
+			try {
+				if (recipientType.equals(RecipientType.TO)) {
+					helper.addTo(identity.getPrincipal());
+				}
+				else if (recipientType.equals(RecipientType.CC)) {
+					helper.addCc(identity.getPrincipal());
+				}
+				else if (recipientType.equals(RecipientType.BCC)) {
+					helper.addBcc(identity.getPrincipal());
+				}
+				else {
+					logger.error("Unable to add recipients.");
+				}
+			} catch (Exception e) {
+				logger.error("Unable to add recipients.");
+			}
+		}
+	}
+
+	private JavaMailSender javaMailSender;
+	private MimeMessageHelper helper;
+
+	@javax.annotation.Resource
+	public void setJavaMailSender(JavaMailSender javaMailSender) {
+		this.javaMailSender = javaMailSender;
+	}
+
+	private static final Logger logger = LoggerFactory.getLogger(JavaMailMessageAdapter.class);
+
 }
--- helianto-message/src/main/java/org/helianto/message/sender/SmtpMailSender.java
+++ helianto-message/src/main/java/org/helianto/message/sender/SmtpMailSender.java
@@ -5,6 +5,8 @@ import javax.mail.internet.MimeMessage;
 
 import org.helianto.core.MessageAdapter;
 import org.helianto.core.MessageSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 
 /**
@@ -20,15 +22,23 @@ public class SmtpMailSender implements MessageSender {
 	}
 
 	public String sendMessage(MessageAdapter<?> message) {
-		javaMailSender.send((MimeMessage) message.getMessage());
+		MimeMessage mimeMessage = (MimeMessage) message.getMessage();
+		try {
+			javaMailSender.send(mimeMessage);
+			logger.debug("E-mail sent from {}.", mimeMessage.getFrom().toString());
+		} catch (Exception e) {
+			logger.error("Unable to send e-mail!");
+		}
 		return "";
 	}
-	
-    private JavaMailSender javaMailSender;
-    
-    @Resource(name="javaMailSender")
-    public void setJavaMailSender(JavaMailSender javaMailSender) {
+
+	private JavaMailSender javaMailSender;
+
+	@Resource(name = "javaMailSender")
+	public void setJavaMailSender(JavaMailSender javaMailSender) {
 		this.javaMailSender = javaMailSender;
 	}
+	
+	private static final Logger logger = LoggerFactory.getLogger(SmtpMailSender.class);
 
 }
