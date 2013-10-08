bc2637c7082ec7beae5e2549db8392356d4af763
Mauricio Fernandes de Castro <mauricio@iservport.com>
2013-10-08 05:29:35 -0300

    Added item counter.

---
+++ helianto-core/src/main/java/org/helianto/core/repository/ItemCounter.java
@@ -0,0 +1,57 @@
+package org.helianto.core.repository;
+
+import java.io.Serializable;
+
+/**
+ * Generic class to count categories.
+ * 
+ * @author mauriciofernandesdecastro
+ */
+public class ItemCounter<B> 
+	implements Serializable
+{
+
+	private static final long serialVersionUID = 1L;
+	private B baseClass;
+	private long itemCount;
+	
+	/**
+	 * Constructor.
+	 */
+	public ItemCounter() {
+		super();
+	}
+	
+	/**
+	 * Full constructor.
+	 * 
+	 * @param baseClass
+	 * @param itemCount
+	 */
+	public ItemCounter(B baseClass, long itemCount) {
+		this();
+		setBaseClass(baseClass);
+		setItemCount(itemCount);
+	}
+	
+	/**
+	 * Base class.
+	 */
+	public B getBaseClass() {
+		return baseClass;
+	}
+	public void setBaseClass(B baseClass) {
+		this.baseClass = baseClass;
+	}
+	
+	/**
+	 * Count.
+	 */
+	public long getItemCount() {
+		return itemCount;
+	}
+	public void setItemCount(long itemCount) {
+		this.itemCount = itemCount;
+	}
+
+}
