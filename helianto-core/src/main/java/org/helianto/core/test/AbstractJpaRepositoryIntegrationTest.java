4c3e8b3e974ba173a79a4255deefa7adc268f89c
Mauricio Fernandes de Castro <mauricio@iservport.com>
2013-12-05 10:06:29 -0200

    Updated configuration.

--- helianto-core/src/main/java/org/helianto/core/test/AbstractJpaRepositoryIntegrationTest.java
+++ helianto-core/src/main/java/org/helianto/core/test/AbstractJpaRepositoryIntegrationTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.Serializable;
 import java.util.List;
 
-import org.helianto.core.config.HeliantoConfig;
+import org.helianto.core.config.HeliantoServiceConfig;
 import org.helianto.core.data.FilterRepository;
 import org.helianto.core.domain.Entity;
 import org.helianto.core.domain.Operator;
@@ -33,7 +33,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @param <T> target class
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes={TestDataSourceConfig.class, HeliantoConfig.class})
+@ContextConfiguration(classes={TestDataSourceConfig.class, HeliantoServiceConfig.class})
 @ActiveProfiles("standalone")
 @Transactional
 public abstract class AbstractJpaRepositoryIntegrationTest<T, R extends FilterRepository<T, Serializable>> {
--- helianto-core/src/test/java/org/helianto/user/service/UserMgrImplIntegrationTests.java
+++ helianto-core/src/test/java/org/helianto/user/service/UserMgrImplIntegrationTests.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
-import org.helianto.core.config.HeliantoConfig;
+import org.helianto.core.config.HeliantoServiceConfig;
 import org.helianto.core.config.MessageTestConfig;
 import org.helianto.core.test.TestDataSourceConfig;
 import org.helianto.user.UserMgr;
@@ -18,7 +18,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes={TestDataSourceConfig.class, HeliantoConfig.class, MessageTestConfig.class})
+@ContextConfiguration(classes={TestDataSourceConfig.class, HeliantoServiceConfig.class, MessageTestConfig.class})
 @ActiveProfiles("standalone")
 @Transactional
 public class UserMgrImplIntegrationTests {
