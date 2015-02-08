d18794bac24274f20acb0ce123ed6b329ddae714
Mauricio Fernandes de Castro <mauricio@iservport.com>
2015-02-08 08:59:48 -0200

    No longer uses freemarker.

--- helianto-core/src/main/java/org/helianto/core/config/HeliantoConfig.java
+++ helianto-core/src/main/java/org/helianto/core/config/HeliantoConfig.java
@@ -1,14 +1,11 @@
 package org.helianto.core.config;
 
-import java.util.Properties;
-
 import org.helianto.core.filter.FilterNamingConventionStrategy;
 import org.helianto.core.naming.internal.DefaultNamingConventionStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -36,26 +33,6 @@ public class HeliantoConfig {
 	@Bean
 	public FilterNamingConventionStrategy filterNamingConventionStrategy() {
 		return new FilterNamingConventionStrategy();
-	}
-	
-	/**
-	 * Freemarker configuration factory.
-	 */
-	@Bean
-	public FreeMarkerConfigurationFactoryBean freeMarkerConfigurationFactory() {
-		FreeMarkerConfigurationFactoryBean freeMarkerConfigurationFactory = 
-				new FreeMarkerConfigurationFactoryBean();
-		freeMarkerConfigurationFactory.setPreferFileSystemAccess(false);
-		freeMarkerConfigurationFactory.setTemplateLoaderPaths(
-				new String[] {"classpath:freemarker/"
-						,"/WEB-INF/classes/freemarker/"
-						,"/WEB-INF/freemarker/"} );
-		Properties settings = new Properties();
-			settings.put("default_encoding", "ISO-8859-1");
-			settings.put("number_format", "computer");
-			settings.put("whitespace_stripping", "true");
-		freeMarkerConfigurationFactory.setFreemarkerSettings(settings);
-		return freeMarkerConfigurationFactory;
 	}
 	
 	@Bean
