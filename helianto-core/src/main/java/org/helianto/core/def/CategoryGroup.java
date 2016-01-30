50727575d40d2c96123c8d2063505900d0ce5021
Mauricio Fernandes de Castro <mauricio@iservport.com>
2016-01-30 10:24:27 -0200

    Added category group for health, added codes to help organize categories into a PDCL logic.

--- helianto-core/src/main/java/org/helianto/core/def/CategoryGroup.java
+++ helianto-core/src/main/java/org/helianto/core/def/CategoryGroup.java
@@ -14,85 +14,93 @@ public enum CategoryGroup implements KeyNameAdapter {
 	/**
 	 * Not defined.
 	 */
-	NOT_DEFINED('_'),
+	NOT_DEFINED('_', "NONE"),
 	/**
 	 * Entity.
 	 */
-	ENTITY('N'),
-	/**
-	 * Document.
-	 */
-	DOCUMENT('D'),
-	/**
-	 * Unit.
-	 */
-	UNIT('U'),
-	/**
-	 * Instrument.
-	 */
-	INSTRUMENT('I'),
-	/**
-	 * Maintenance.
-	 */
-	MAINTENANCE('M'),
-	/**
-	 * Stock.
-	 */
-	STOCK('S'),
-	/**
-	 * Process.
-	 */
-	PROCESS('O'),
-	/**
-	 * Product.
-	 */
-	PRODUCT('P'),
-	/**
-	 * Order.
-	 */
-	ORDER('Y'),
-	/**
-	 * Service.
-	 */
-	SERVICE('E'),
-	/**
-	 * Planning.
-	 */
-	REQUEST('Q'),
+	ENTITY('N', "NONE"),
 	/**
 	 * Project.
 	 */
-	PROJECT('J'),
+	PROJECT('J', "PLAN"),
 	/**
 	 * Planning.
 	 */
-	PLANNING('R'),
+	PLANNING('R', "PLAN"),
 	/**
-	 * User.
+	 * Action.
 	 */
-	USER('Z'),
+	ACTION('A', "PLAN"),
+	/**
+	 * Health.
+	 */
+	HEALTH('H', "PLAN"),
+	/**
+	 * Document.
+	 */
+	DOCUMENT('D', "DO"),
+	/**
+	 * Product.
+	 */
+	PRODUCT('P', "DO"),
+	/**
+	 * Service.
+	 */
+	SERVICE('E', "DO"),
+	/**
+	 * Process.
+	 */
+	PROCESS('O', "DO"),
+	/**
+	 * Order.
+	 */
+	ORDER('Y', "DO"),
+	/**
+	 * Instrument.
+	 */
+	INSTRUMENT('I', "CONTROL"),
+	/**
+	 * Maintenance.
+	 */
+	MAINTENANCE('M', "CONTROL"),
+	/**
+	 * Stock.
+	 */
+	STOCK('S', "CONTROL"),
 	/**
 	 * Labor.
 	 */
-	LABOR('L'),
+	LABOR('L', "CONTROL"),
+	/**
+	 * User.
+	 */
+	USER('Z', "CONTROL"),
+	/**
+	 * Planning.
+	 */
+	REQUEST('Q', "LEARN"),
 	/**
 	 * Action.
 	 */
-	KNOWLEDGE('K'),
-	/**
-	 * Action.
-	 */
-	ACTION('A'),
+	KNOWLEDGE('K', "LEARN"),
 	/**
 	 * Competence.
 	 */
-	COMPETENCE('C');
+	COMPETENCE('C', "LEARN"),
+	/**
+	 * Unit.
+	 */
+	UNIT('U', "NONE")
+	;
 	
-	private CategoryGroup(char value) {
+	private CategoryGroup(char value, String code) {
 		this.value = value;
+		this.code = code;
 	}
 	
 	private char value;
+	
+	private String code;
 	
 	public char getValue() {
 		return value;
@@ -104,7 +112,7 @@ public enum CategoryGroup implements KeyNameAdapter {
 	
 	@Override
 	public String getCode() {
-		return value+"";
+		return code;
 	}
 	
 	@Override
