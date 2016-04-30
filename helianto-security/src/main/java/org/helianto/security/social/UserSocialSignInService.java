a6a8406de9bcab882dce88e2d0abe1cc1be35757
Mauricio Fernandes de Castro <mauricio@iservport.com>
2016-04-30 15:38:29 -0300

    0.6.2.2 Development release.
    Added Spring Social helpers.

---
+++ helianto-security/src/main/java/org/helianto/security/social/UserSocialSignInService.java
@@ -0,0 +1,26 @@
+package org.helianto.security.social;
+
+import javax.inject.Inject;
+
+import org.helianto.security.internal.UserDetailsAdapter;
+import org.helianto.security.util.SignInUtils;
+import org.helianto.user.domain.User;
+import org.helianto.user.repository.UserRepository;
+
+/**
+ * Required to sign in users after social providers.
+ *  
+ * @author mauriciofernandesdecastro
+ */
+public class UserSocialSignInService {
+	
+	@Inject
+	private UserRepository userRepository;
+	
+	public void signin(Integer userId){
+		User user = userRepository.findOne(userId);
+		UserDetailsAdapter userDetails = new UserDetailsAdapter(user);
+		SignInUtils.signin(userDetails);
+	}
+
+}
