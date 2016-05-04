bef040f813bdf69b7b9d9d2581e6dc8fd3870128
Mauricio Fernandes de Castro <mauricio@iservport.com>
2016-05-04 17:36:06 -0300

    Added registration form.

---
+++ helianto-security/src/main/java/org/helianto/security/internal/Registration.java
@@ -0,0 +1,106 @@
+package org.helianto.security.internal;
+
+/**
+ * Registration form.
+ * 
+ * @author mauriciofernandesdecastro
+ */
+public class Registration {
+
+    private Integer contextId = 1;
+
+    private String email;
+
+    private  String password;
+
+    private String entityAlias;
+
+    private boolean admin;
+
+    private boolean isDomain;
+
+    private Integer cityId = 0;
+
+    public Registration(){
+      super();
+    }
+
+    public Registration(Integer cityId, Integer contextId, String email, String entityAlias, Boolean isAdmin, Boolean isDomain, String password) {
+        this.cityId = cityId;
+        this.contextId = contextId;
+        this.email = email;
+        this.entityAlias = entityAlias;
+        this.admin = isAdmin;
+        this.isDomain = isDomain;
+        this.password = password;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public Integer getContextId() {
+        return contextId;
+    }
+
+    public void setContextId(Integer contextId) {
+        this.contextId = contextId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEntityAlias() {
+        return entityAlias;
+    }
+
+    public void setEntityAlias(String entityAlias) {
+        this.entityAlias = entityAlias;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public boolean isDomain() {
+        return isDomain;
+    }
+
+    public void setDomain(boolean domain) {
+        this.isDomain = domain;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Registration{" +
+                "admin=" + admin +
+                ", contextId=" + contextId +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", entityAlias='" + entityAlias + '\'' +
+                ", isDomain=" + isDomain +
+                ", cityId=" + cityId +
+                '}';
+    }
+}
